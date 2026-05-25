39702a4823dab1287c9185601742b1edfc264e9d
zaxwtf <zaxwtf06@gmail.com>
2026-05-25 10:53:03 +0200

    feat: new script for the landing page, added function to switch to the Pokedex page by clicking on its button in the menu

---
+++ JS/landingScript.js
@@ -0,0 +1,6 @@
+const pokedexBtn = document.querySelector("#landing-pokedex")
+
+//llevar a la pokedex al pulsar
+pokedexBtn.addEventListener("click", () => {
+    return window.location.href = "pokedex.html"
+})
