deef43fff396bb757a334ce4ec2be712eb6c84fd
zaxwtf <zaxwtf06@gmail.com>
2026-06-04 10:26:27 +0200

    feat: agregada funcion para ir a pagina de items desde la landing

--- JS/landingScript.js
+++ JS/landingScript.js
@@ -1,6 +1,14 @@
-const pokedexBtn = document.querySelector("#landing-pokedex")
+const pokedexBtn = document.querySelector("#landing-pokedex");
+const itemsBtn = document.querySelector('#landing-objects');
 
 //llevar a la pokedex al pulsar
 pokedexBtn.addEventListener("click", () => {
     return window.location.href = "pokedex.html"
-})
+});
+
+
+//llevar a objetos al pulsar
+itemsBtn.addEventListener('click', () =>{
+    return window.location.href = 'items.html'
+});
+
